a4a999fa5ad1fa5afb2301f77be13797f8d2fa9a
Rose Le <rosehle@protonmail.com>
2023-01-25 13:12:02 -0800

    Set up html files, used express.static and passed in directory

---
+++ node-course/web-server/src/app.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const express = require("express");
+
+// console.log(__dirname);
+// console.log(path.join(__dirname, "../public"));
+
+const app = express();
+
+publicFolderPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicFolderPath));
+
+//req = obj containing info about request
+//res = obj containing methods to use in response
+// app.get("", (req, res) => {
+//   res.send("<h1>Weather</h1>");
+// });
+
+// app.get("/help", (req, res) => {
+//   res.send({
+//     name: "Jeff",
+//     age: 26,
+//   });
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("<h1>About page<h1>");
+// });
+
+// app.get("/weather", (req, res) => {
+//   res.send({
+//     forecast: "It is sunny",
+//     location: "Encinitas",
+//   });
+// });
+
+//start up server + specify port
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
